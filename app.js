510c95b22baba287392ff531d038b88c0c241ba8
revanth.g <revanth.g@inxsasia.com>
2021-03-13 13:09:37 +0530

    initial

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+let port = process.env.PORT || 8000;
 // parse application/json
 app.use(bodyParser.json())
 
@@ -21,7 +21,7 @@ router.post("/postback", (req, res) => {
 
 app.use("/", router);
 
-app.listen(8000, (err) => {
+app.listen(port, (err) => {
     console.log("server started");
     Logger.info(JSON.stringify("server started"));
 })
